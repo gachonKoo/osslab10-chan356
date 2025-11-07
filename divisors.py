196d6325c31dfd0777409cd4aded7755dff34438
chan356 <leechanho1226@gachon.ac.kr>
2025-11-07 16:13:40 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,12 +1,12 @@
-def divisors(n: int):
-  
-    return [i for i in range(1, n + 1) if n % i == 0]
+import sys
 
+def divisors(n: int):
+    return [i for i in range(1, n + 1) if n % i == 0]
 
 def get_divisors(n: int):
     return divisors(n)
 
 if __name__ == "__main__":
- 
-    n = int(input())
+    
+    n = int(sys.argv[1])
     print(" ".join(str(x) for x in divisors(n)))
