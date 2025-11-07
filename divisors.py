1a1387c31fc3440d0cc15b8da9a549bd4e1c3c52
chan356 <leechanho1226@gachon.ac.kr>
2025-11-07 16:06:19 +0900

    Refactor divisor calculation into functions

--- divisors.py
+++ divisors.py
@@ -1,8 +1,10 @@
-n = int(input())
-divs = []
+def divisors(n: int):
+    return [i for i in range(1, n + 1) if n % i == 0]
 
-for i in range(1, n + 1):
-    if n % i == 0:
-        divs.append(str(i))
 
-print(" ".join(divs))
+def get_divisors(n: int):
+    return divisors(n)
+
+if __name__ == "__main__":
+    n = int(input())
+    print(" ".join(str(x) for x in divisors(n)))
